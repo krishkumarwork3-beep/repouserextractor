7c68854dd4f93a946e25ab8bdbb7997fa1f52f68
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-08-18 18:16:05 +0530

    Add AWS deployment configuration variables

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 set -x
+
+aws_account_id=$(aws sts get-caller-identity \
+    --query 'Account' \
+    --output text)
+
+echo "AWS Account ID: $aws_account_id"
+
+aws_region="ap-south-1"
+bucket_name="krish1234554321"
+lambda_func_name="s3-lambda-function"
+role_name="s3-lambda-sns"
+email_address="xyz@gmail.com"
