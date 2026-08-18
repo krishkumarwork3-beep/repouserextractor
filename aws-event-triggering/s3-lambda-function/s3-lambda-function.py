5d3a951f6cfb95c10f842474a63f76f14b2e53a8
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-08-18 18:00:04 +0530

    Publish S3 upload notification through SNS

--- aws-event-triggering/s3-lambda-function/s3-lambda-function.py
+++ aws-event-triggering/s3-lambda-function/s3-lambda-function.py
@@ -10,6 +10,14 @@ def lambda_handler(event, context):
 
     sns_client = boto3.client('sns')
 
+    topic_arn = 'arn:aws:sns:us-east-1:<account-id>:s3-lambda-sns'
+
+    sns_client.publish(
+        TopicArn=topic_arn,
+        Subject='S3 Object Created',
+        Message=f"File '{object_key}' was uploaded to bucket '{bucket_name}'"
+    )
+
     return {
         'statusCode': 200,
         'body': json.dumps('Lambda function executed successfully')
