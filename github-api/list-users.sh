b854dfcfc1fc21322a36f9d7a3ab46e443e45f66
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-08-18 18:57:25 +0530

    Add function to list repository collaborators

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -19,3 +19,9 @@ function github_api_get {
 
     curl -s -u "${USERNAME}:${TOKEN}" "$url"
 }
+
+
+# Function to list users with read access
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+}
